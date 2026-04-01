9b0e9a2dd779724aa6f47e3d27ea0efefe0aa684
mdanieledcg-pixel <mdaniele.dcg@gmail.com>
2026-04-01 12:50:42 -0400

    Create pairings.ts

---
+++ app/app/lib/pairings.ts
@@ -0,0 +1,40 @@
+export type Pairing = {
+  seed: number
+  playerA: string
+  playerB: string
+}
+
+export const PAIRINGS: Pairing[] = [
+  { seed: 1, playerA: "Bobby Taylor", playerB: "Dan Bevis" },
+  { seed: 2, playerA: "Gerard Rosato", playerB: "Mark Sessa" },
+  { seed: 3, playerA: "Brian Hungarter", playerB: "Scott Minor" },
+  { seed: 4, playerA: "Michael Dafnis", playerB: "Charles Franzone" },
+  { seed: 5, playerA: "Pete Gaumer", playerB: "Brian Ambron" },
+  { seed: 6, playerA: "Grant Morse", playerB: "Rob Sheahan" },
+  { seed: 7, playerA: "Jim Kingkiner", playerB: "Mike McHale" },
+  { seed: 8, playerA: "Alex Bradbury", playerB: "John DiJiosia" },
+  { seed: 9, playerA: "Shane Stolzer", playerB: "John Boyce" },
+  { seed: 10, playerA: "Brian Sutcliffe", playerB: "Steve Purdy" },
+  { seed: 11, playerA: "Chip Studer", playerB: "Chris Martin" },
+  { seed: 12, playerA: "B.J. Murray", playerB: "Matt Daniele" },
+  { seed: 13, playerA: "Eric Pasternack", playerB: "Bill Weychert" },
+  { seed: 14, playerA: "Coleman Hauber", playerB: "Brian Callahan" },
+  { seed: 15, playerA: "Ted Swain", playerB: "Sam Donahue" },
+  { seed: 16, playerA: "Rodney Anders", playerB: "Steve Morton" },
+  { seed: 17, playerA: "Chandler Thompson", playerB: "Mike Siniscalchi" },
+  { seed: 18, playerA: "Luke Hohenstein", playerB: "Brian Spigelmyer" },
+  { seed: 19, playerA: "Brian Lyden", playerB: "Jason Law" },
+  { seed: 20, playerA: "Stu Levick", playerB: "Pete Oh" },
+  { seed: 21, playerA: "Colin Goshert", playerB: "Bob Eschenbach" },
+  { seed: 22, playerA: "Jeff Zamorski", playerB: "John Prendergast" },
+  { seed: 23, playerA: "Justin Morasco", playerB: "Heath Wawrzynek" },
+  { seed: 24, playerA: "Chris Albright", playerB: "John Carroll" },
+  { seed: 25, playerA: "Tim Edwards", playerB: "Brian Roach" },
+  { seed: 26, playerA: "Mike Caputo", playerB: "Brian Eckelmeyer" },
+  { seed: 27, playerA: "Vince Davenport", playerB: "Tim Brennan" },
+  { seed: 28, playerA: "Shane Dolan", playerB: "Brent Davis" },
+  { seed: 29, playerA: "Ryan Smith", playerB: "Tripp Hilles" },
+  { seed: 30, playerA: "Steve Latos", playerB: "Nick Cangelosi" },
+  { seed: 31, playerA: "Greg Coletto", playerB: "Joe Nguyen" },
+  { seed: 32, playerA: "Matt Mingione", playerB: "Paul Morrow" },
+]
